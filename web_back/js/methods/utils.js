077b0245ea863cbcd1a5bd3285977d340e335986
pjh1123 <2837753208@qq.com>
2019-09-23 15:44:43 +0800

    1-原有分支，不全

--- web_back/js/methods/utils.js
+++ web_back/js/methods/utils.js
@@ -3,5 +3,8 @@ var Utitle = 'http://localhost:8000'
 var URL = {
     ulogin_url:Utitle+'/admin/login',
     ulogout_url:Utitle+'/admin/logout',
-
+ 
+    article_addurl:Utitle+'/admin/category_add',
+    article_delurl:Utitle+'/admin/category_delet',
+    article_changeurl:Utitle+'/admin/category_edit', 
 }
